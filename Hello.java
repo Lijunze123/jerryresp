52d0efa138060156b6b8b93a2b3380a5e5de6272
jerry <leejay520@foxmail.com>
2018-06-22 21:03:47 +0800

    add new file to local master

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ package com.jerry.test;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Jerry, come on");
 	}
 
 }
